47979f9a785315e97e3fe88a189861d6fcd63e03
Chris Chamberlain <c.chamberlain@iinet.net.au>
2017-07-10 23:19:28 +1000

    Implement core scrollback functions
    This is accomplished by replacing the `Vec` buffer with a `VecDeque`.
    
    The *active region* represents the region in the buffer that is modifiable. It is essentially the latest `self.num_lines()` lines of the buffer.
    
    Upon start up, the *visible region* is equal to the active region, and when new lines are added the visible/active region both move accordingly. However when you scroll, the visible region changes. When you type in new commands again tho, the visible region jumps back to equal the active region.

--- src/config.rs
+++ src/config.rs
@@ -360,7 +360,7 @@ impl de::Deserialize for ActionWrapper {
             type Value = ActionWrapper;
 
             fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
-                f.write_str("Paste, Copy, PasteSelection, or Quit")
+                f.write_str("Paste, Copy, PasteSelection, Quit, ScrollUp or ScrollDown")
             }
 
             fn visit_str<E>(self, value: &str) -> ::std::result::Result<ActionWrapper, E>
@@ -371,6 +371,8 @@ impl de::Deserialize for ActionWrapper {
                     "Copy" => Action::Copy,
                     "PasteSelection" => Action::PasteSelection,
                     "Quit" => Action::Quit,
+                    "ScrollUp" => Action::ScrollUp,
+                    "ScrollDown" => Action::ScrollDown,
                     _ => return Err(E::invalid_value(Unexpected::Str(value), &self)),
                 }))
             }

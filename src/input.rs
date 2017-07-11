ef5215db117d6cf043a69f2bba71a2d5bb79843a
Chris Chamberlain <c.chamberlain@iinet.net.au>
2017-07-11 23:24:08 +1000

    Beginning to fix tests
    Now they compile.. Running: they all fail atm :D

--- src/input.rs
+++ src/input.rs
@@ -558,7 +558,7 @@ mod tests {
     use term::{SizeInfo, Term, TermMode, mode};
     use event::{Mouse, ClickState};
     use config::{self, Config, ClickHandler};
-    use index::{Point, Side};
+    use index::{Point, Side, AbsoluteLine};
     use selection::Selection;
 
     use super::{Action, Binding, Processor};
@@ -621,15 +621,24 @@ mod tests {
         fn mouse_mut(&mut self) -> &mut Mouse {
             self.mouse
         }
+
         fn received_count(&mut self) -> &mut usize {
             &mut self.received_count
         }
+
         fn suppress_chars(&mut self) -> &mut bool {
             &mut self.suppress_chars
         }
+        
         fn last_modifiers(&mut self) -> &mut ModifiersState {
             &mut self.last_modifiers
         }
+
+        fn move_visible_region_up(&mut self, lines: AbsoluteLine) {}
+
+        fn move_visible_region_down(&mut self, lines: AbsoluteLine) {}
+        
+        fn jump_to_bottom(&mut self) {}
     }
 
     macro_rules! test_clickstate {
